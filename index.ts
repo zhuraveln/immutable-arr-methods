6ec65e9d03b80ec2b55bd20a5512692a93ff49a2
Zhuravel Nikita <zhuravel.nikita@gmail.com>
2022-12-23 18:27:17 +0300

    Removed all custom methods

--- index.ts
+++ index.ts
@@ -1,38 +1,5 @@
 /** Class for immutable work with array */
 export class immArr {
-  /** Immutable inserts element into array
-   * @return new array
-   */
-  static insert<Type>(array: Type[], index: number, element: Type): Type[] {
-    return [...array.slice(0, index), element, ...array.slice(index)];
-  }
-
-  /** Immutable remove element from array
-   * @return new array
-   */
-  static remove<Type>(array: Type[], index: number): Type[] {
-    return [...array.slice(0, index), ...array.slice(index + 1)];
-  }
-
-  /** Immutable replace element in array
-   * @return new array
-   */
-  static replace<Type>(array: Type[], index: number, element: Type): Type[] {
-    return [...this.insert(this.remove(array, index), index, element)];
-  }
-
-  /** Immutable change positions for two elements in array
-   * @return new array
-   */
-  static swap<Type>(
-    array: Type[],
-    sourceIndex: number,
-    destinationIndex: number,
-    element: Type,
-  ): Type[] {
-    return [...this.insert(this.remove(array, sourceIndex), destinationIndex, element)];
-  }
-
   /** Return last element from array without immutable source array
    * @return last element
    */
