3d0ff8bd4626d18417261c7e5ef7e5d6104316b1
Zhuravel Nikita <zhuravel.nikita@gmail.com>
2022-12-25 12:39:15 +0300

    Added push, shift, unshift methods

--- index.ts
+++ index.ts
@@ -1,9 +1,47 @@
-/** Class for immutable work with array */
-export class immArr {
-  /** Return last element from array without immutable source array
-   * @return last element
-   */
-  static pop<Type>(array: Type[]): Type {
-    return array[array.length - 1];
-  }
+//----------------------------------------------------------------
+// POP
+
+/** Return last element from array without mutable source array
+ * @param {array<T>} array - source array
+ * @return {element<T>} last element of array
+ */
+export function pop<Type>(array: Type[]): Type {
+  return array[array.length - 1]
 }
+
+//----------------------------------------------------------------
+// PUSH
+
+/** Return new array with new a elements at end of array without mutable source array
+ * @param {array<T>} array - source array
+ * @param {elements<T>} elements - array of a new elements to push
+ * @return {array<T>} new array
+ */
+export function push<Type>(array: Type[], ...elements: Type[]): Type[] {
+  return [...array, ...elements]
+}
+
+//----------------------------------------------------------------
+// SHIFT
+
+/** Return first element from array without mutable source array
+ * @param {array<T>} array - source array
+ * @return {element<T>} first element of array
+ */
+export function shift<Type>(array: Type[]): Type {
+  return array[0]
+}
+
+//----------------------------------------------------------------
+// UNSHIFT
+
+/** Return new array with new a elements at start of array without mutable source array
+ * @param {array<T>} array - source array
+ * @param {elements<T>} elements - array of a new elements to push
+ * @return {array<T>} new array
+ */
+export function unshift<Type>(array: Type[], ...elements: Type[]): Type[] {
+  return [...elements, ...array]
+}
+
+//----------------------------------------------------------------
